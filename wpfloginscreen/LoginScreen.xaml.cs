79d646fbd956f85dae55d4c2df3031fd5025369a
JasperF20 <jasperdewaard@hotmail.com>
2018-06-04 13:21:55 +0200

    mainscrreen webshop + save account database
    Bij inloggen schrijft die hard coded een account naar de database
    Makkelijke fix naar registratie
    Mainscreen opzet gemaakt

---
+++ wpfloginscreen/LoginScreen.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Linq;
+using System.Data.Entity.Validation;
+
+// download sql database: https://www.microsoft.com/en-US/download/details.aspx?id=29062
+// kies voor SQLEXPRADV_x64_ENU.exe
+// tutorial: https://docs.microsoft.com/en-us/sql/relational-databases/lesson-1-connecting-to-the-database-engine?view=sql-server-2017
+
+namespace wpfloginscreen
+{
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        Registration registrationPage = new Registration();
+        MainWindow webshopPage = new MainWindow();
+
+        public int CreateUserID()
+        {
+            int x = 10;
+            return x;
+        }
+
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text) || string.IsNullOrWhiteSpace(txtPassword.Password))
+            {
+                MessageBox.Show("Verkeerde inloggegevens!");
+            }
+            else
+            {
+                try
+                {
+                    using (var db = new webshopHostEntities())
+                    {
+                        //hardcoded nieuwe user aanmaken om te testen
+                        // deze moet opgehaald worden uit invoergegevens
+                        var user = new User
+                        {
+                            User_ID = 1,
+                            Username = txtUsername.Text,
+                            Password = txtPassword.Password,
+                            Credit = 50
+                        };
+
+                        db.Users.Add(user);
+                        db.SaveChanges();
+                        MessageBox.Show("Account created & login succesfull!");
+
+                        /*
+                        var query = from u in db.Users
+                                    orderby u.Username
+                                    select u;
+                        */
+
+                        Close();
+                        webshopPage.Show();
+
+                    }
+                }
+                catch (Exception exception)
+                {
+                 
+                }
+
+
+
+            
+            }
+        }
+
+        private void ButtonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            registrationPage.Show();
+            Close();
+        }
+
+    }
+}
+
+
+
+
---
+++ wpfloginscreen/proeftentamen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace wpfloginscreen
+{
+    //opgave 1
+    class Student
+    {
+        public DateTime GD { get; }
+        public int leeftijd { get { (DateTime.Now - GD).getYear()} }
+
+        public Student(DateTime gd)
+        {
+            this.GD = gd;
+        }
+
+    }
+
+    // opdracht 3
+    public class LetterReeks : IEnumerable<char>
+    {
+        char eerste, laatste;
+        public LetterReeks (char e, char l) {
+            e = eerste;
+            l = laatste;
+        }
+
+        public IEnumerator<char> getEnemurator()
+        {
+            for (char c = eerste; c < laatste; c++)
+            {
+                // doe iets
+            }
+        }
+    }
+
+
+    //vraag 4
+    var result = from h in holidays.Descendants("holiday")
+                 where h.Parent.hasAttribute(a => a.value = 2010)
+                 select h.attribute("day").value
+}
+*/
