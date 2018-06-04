fd762838b55000eeb5fcfe51a85cf541fee5ba5f
Jeroen Boshuis <jeroen.boshuis@student.hu.nl>
2018-06-04 13:10:04 +0200

    Pushed out of the .rar file
    Pushed the project not in the .rar file, which is a bit more comprehensive

---
+++ wpfloginscreen/wpfloginscreen/LoginScreen.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Linq;
+using System.Data.Entity.Validation;
+
+// download sql database: https://www.microsoft.com/en-US/download/details.aspx?id=29062
+// kies voor SQLEXPRADV_x64_ENU.exe
+// tutorial: https://docs.microsoft.com/en-us/sql/relational-databases/lesson-1-connecting-to-the-database-engine?view=sql-server-2017
+
+namespace wpfloginscreen
+{
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        Registration registrationPage = new Registration();
+        MainWindow webshopPage = new MainWindow();
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text) || string.IsNullOrWhiteSpace(txtPassword.Password))
+            {
+                MessageBox.Show("Verkeerde inloggegevens!");
+            }
+            else
+            {
+                try
+                {
+                    using (var db = new webshopHostEntities())
+                    {
+                        //hardcoded nieuwe user aanmaken om te testen
+                        // deze moet opgehaald worden uit invoergegevens
+                        var user = new User
+                        {
+                            User_ID = 1,
+                            Username = "Admin",
+                            Password = "Admin"
+                        };
+
+                        db.Users.Add(user);
+                        db.SaveChanges();
+
+                        var query = from b in db.Users
+                                    orderby b.Username
+                                    select b;
+
+                        Console.WriteLine(" All student in the database: ");
+
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Username);
+                        }
+
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception exception)
+                {
+                }
+
+
+
+            
+            }
+        }
+
+        private void ButtonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            registrationPage.Show();
+            Close();
+        }
+
+    }
+}
+
+
+
+
---
+++ wpfloginscreen/wpfloginscreen/Registration.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfloginscreen
+{
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        public static string ReverseIt(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private void generate_password(object sender, RoutedEventArgs e)
+        {
+            //TODO: deze methode maakt een nieuwe user aan in de database, nog niet af!
+            // get waarde van username textfield
+            // zet die string achterstevoren
+            // laat het wachtwoord zien in het wachtwoord textfield
+            // combinatie van username en password naar sql database uploaden
+            // laat berichtje zien dat account is aangemaakt
+
+            if(string.IsNullOrWhiteSpace(regUsername.Text))
+            {
+                MessageBox.Show("Geen username ingevuld", "Systeem melding");
+            } else
+            {
+                var usernameReversed = ReverseIt(regUsername.Text);
+                regPassword.AppendText(usernameReversed);
+                MessageBox.Show("Account aangemaakt!", "Systeem melding");
+            }
+            /*
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=localhost\sqle2012; Initial Catalog=LoginDB; Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tblUser WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", regUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", regPassword.Password);
+
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+            */
+        }
+
+        private void To_Login_Click(object sender, RoutedEventArgs e)
+        {
+            //loginPage.Show();
+           // Close();
+        }
+
+    }
+}
